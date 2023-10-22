0e3e0df13acf71ead763007e2b14e643d620f41b
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-10-23 01:04:20 +0530

    misc : Minor runtime fixes

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,28 @@
-import uvicorn
+""" This module demonstrates how to create a FastAPI App,
+connect it to SurrealDB
+"""
+from surrealdb import Surreal
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI()
+
+@asynccontextmanager
+async def surreal_db():
+    """
+    Function that connects to a local database endpoint,
+    signs this connection in to a specific authentication scope,
+    and switches to a specific namespace and database
+    """
+    db = Surreal("http://localhost:8000")
+    db.connect()
+    await db.signin({"user": "root", "pass": "root"})
+    await db.use("starter", "todo_backend")
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
